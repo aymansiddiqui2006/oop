0cc3f549fea68e524bb7bcae7fbcae4abd4cee8b
Your Nameaymansiddiqui2006 <aymansiddiqui2006@gmail.com>
2025-12-09 23:10:00 +0530

    inline function

--- exam practice/tempCodeRunnerFile.cpp
+++ exam practice/tempCodeRunnerFile.cpp
@@ -1,2 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+inline int Max(int a,int b){
+    return (a>b)?a:b;
+}
+int main (){
+
+    cout<<Max(9,7)<<endl;
+    cout<<Max(9,90)<<endl;
+    cout<<Max(6,6)<<endl;
+    cout<<Max(5,7)<<endl;
+  return 0;
 }
