8367e84b00762d6acd9886373451cb5227d369f7
Your Nameaymansiddiqui2006 <aymansiddiqui2006@gmail.com>
2025-12-11 02:13:30 +0530

    template

--- exam practice/template.cpp
+++ exam practice/template.cpp
@@ -1,13 +1,30 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-template<typename t>
-void Max(t a,t b){
-    cout<<max(a,b)<<endl;
-}
-int main (){
-int a=9,b=90;
-    Max(a,b);
-    Max<float>(0.0,9.4);
-    Max<double>(90.9,489);
+
+// void Max(t a,t b){
+//     cout<<max(a,b)<<endl;
+// }
+template <typename t>
+class number
+{
+  public:
+  t a ;
+  t b ;
+  number(t x,t y)
+  {
+    a=x;
+    b=y;
+    t result = a + b;
+    cout << result;
+  }
+} ;
+int main()
+{
+  // int a=9,b=90;
+  //     Max(a,b);
+  //     Max<float>(0.0,9.4);
+  //     Max<double>(90.9,489);
+
+  number<int> n(4,8);
   return 0;
 }
